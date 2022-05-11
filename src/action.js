6f8cab16da55d2d9193259eb5946cfb2f0379fc4
Elijah Cannon <Cclayelijah@gmail.com>
2022-05-11 16:47:01 -0600

    secrets config

--- src/action.js
+++ src/action.js
@@ -1,5 +1,11 @@
+const core = require("@actions/core");
+
 async function run() {
-  console.log("hello world");
+  const MC_USERNAME = core.getInput("MC_USERNAME");
+  const MC_PASSWORD = core.getInput("MC_PASSWORD");
+
+  console.log(MC_USERNAME);
+  console.log(MC_PASSWORD);
 }
 
 run();
